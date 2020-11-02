1f73a6c57ec70b5e9128e9e54d75052f29558e9c
Stefano Lupo <slupo@hubspot.com>
2020-11-02 20:48:41 +0000

    After grind

--- linkedlist/ll.c
+++ linkedlist/ll.c
@@ -10,6 +10,16 @@ struct Node {
     int data;
 };
 
+
+/**
+ * [ 0 ] --> [ 15 ] --> [ 16 ] --> [ 17 ] --> [ / ]
+ *  ^
+ *  | 
+ *  current
+ */
+ 
+
+
 // Let head be global
 Node* head = NULL;
 
@@ -23,40 +33,47 @@ void addNodeAfterData(int searchData, int newNodeAfterData);
 void reverseLinkedList();
 
 int main(){
-
-    head = (Node*)malloc(sizeof(Node));
+    
+    // Create the head of the list
+    head = (Node*) malloc(sizeof(Node));
     head -> next = NULL;
     head -> data = 0;
 
     printLinkedList();
     addNode(15);
+    addNode(16);
+    addNode(17);
     printLinkedList();
 
-    Node* searched = searchForNode(15);
+    Node* searched = searchForNode(19);
+
     if(searched != NULL){
         printf("Node found: %d\n",searched->data);
     } else {
         printf("Node not contained in list\n");
     }
 
-    addNodeToStart(99);
+    // addNodeToStart(99);
+    // printLinkedList();
+
+    addNodeAfterData(16, 3);
+    // addNodeAfterData(0,55);
+    // addNode(12);
     printLinkedList();
 
-    addNodeAfterData(0,100);
-    addNodeAfterData(0,55);
-    addNode(12);
-    printLinkedList();
+    // Try add node after 99 which is not in the list
+    addNodeAfterData(99, 3);
 
-    reverseLinkedList();
+    // reverseLinkedList();
 
-    printLinkedList();
-    return 0;
+    // printLinkedList();
+    // return 0;
 }
 
 void printLinkedList(){
     Node* current = head;
     while(current != NULL){
-        printf("%d ->",current->data);
+        printf("%d ->", current->data);
         current = current->next;
     }
     printf("\n");
@@ -64,10 +81,32 @@ void printLinkedList(){
 
 void addNode(int data){
     Node* last = getLastNode();
+
+    // Create a brand new node that will become the tail
     Node* newLast = (Node*)malloc(sizeof(Node));
     newLast->data = data;
     newLast->next = NULL;
+    
+    // Connect the new node to the previous last nodes next
     last->next = newLast;
+}
+
+Node* searchFor(int data) {
+    Node* current = head;
+
+    while(current != NULL) {
+        
+        printf("Checking %d\n", current -> data);
+        if (current -> data == data) {
+            // Found a match, so return this node.
+            return current;
+        }
+
+        current = current -> next;
+    }
+
+    // At this point, we finished iterating through the list, but didn't find the data
+    return NULL;
 }
 
 void addNodeToStart(int data){
@@ -79,9 +118,12 @@ void addNodeToStart(int data){
 
 Node* getLastNode(){
     Node* current = head;
+
     while(current->next != NULL){
+        // We know that we're not at the end of the list as the node's _next_ is not NULL
         current = current->next;
     }
+
     return current;
 }
 
@@ -101,21 +143,27 @@ void addNodeAfterData(int searchData, int newNodeData){
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = newNodeData;
 
+
     Node* nodeAfter = NULL;
     Node* nodeBefore = head;
 
     while(nodeBefore != NULL){
         if(nodeBefore->data == searchData){
+            // Get the next node, regardless of what it is (whethers its an actual node or if its null)
+            // as we need to connect newNode to it 
             nodeAfter = nodeBefore->next;
+
             nodeBefore->next = newNode;
             newNode->next = nodeAfter;
+
             return;
         } else {
             nodeBefore = nodeBefore->next;
         }
     }
-    printf("That node with data %d is not contained in the list\n",searchData);
 
+    // Wen't through entire list and didn't find the search data
+    printf("That node with data %d is not contained in the list\n",searchData);
 }
 
 void reverseLinkedList(){
